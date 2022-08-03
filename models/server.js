bcf6a16780228bb9f0d12c7c5ae731c33888eb31
stiven4950 <omarrocha4950@gmail.com>
2022-08-02 22:45:53 -0500

    Agregadas cabeceras cors en Server

--- models/server.js
+++ models/server.js
@@ -50,6 +50,14 @@ class Server {
             tempFileDir: '/tmp/',
             createParentPath: true
         })); */
+        this.app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+            res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+            next();
+        });
+        
     }
 
     routes() {
